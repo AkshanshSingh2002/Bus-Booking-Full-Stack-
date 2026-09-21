41abfd69ea44da7c7787164533e5a0ac66f41028
Akshansh Singh <akshanshs2000@gmail.com>
2026-09-21 12:16:00 +0530

    Modify Razorpay configuration

--- backend/src/config/razorpay.js
+++ backend/src/config/razorpay.js
@@ -1,7 +1,7 @@
 import Razorpay from "razorpay";
 
-const keyId = process.env.RAZORPAY_ID || process.env.RAZORPAY_KEY_ID;
-const keySecret = process.env.RAZORPAY_SECRET || process.env.RAZORPAY_KEY_SECRET;
+const keyId = process.env.REZORPAY_TEST_API_KEY || process.env.RAZORPAY_KEY_ID;
+const keySecret = process.env.REZORPAY_TEST_KEY_SECRET || process.env.RAZORPAY_KEY_SECRET;
 
 const razorpay = keyId && keySecret
     ? new Razorpay({ key_id: keyId, key_secret: keySecret })
